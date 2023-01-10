51453e2344652b3e6bdc33a1b1ee470a83d0288e
acl <aungcholatt888@gmail.com>
2023-01-10 17:59:44 +0630

    Initial Commit

---
+++ Swift_Video_App/Swift_Video_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_Video_AppApp.swift
+//  Swift_Video_App
+//
+//  Created by macair on 10/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct Swift_Video_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
